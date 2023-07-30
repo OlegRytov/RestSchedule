97dd08b18ceea8aeaacad63a189b91be063900c7
Oleg Rytov <cattimofei@gmail.com>
2023-07-30 11:47:33 +0300

    add Resources

--- src/test/java/ru/netology/service/RestScheduleTest.java
+++ src/test/java/ru/netology/service/RestScheduleTest.java
@@ -2,14 +2,18 @@ package ru.netology.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class RestScheduleTest {
 
     @ParameterizedTest
-    @CsvSource({
+/*    @CsvSource({
             "10000,3000,20000"
     })
+
+ */
+    @CsvFileSource(files = "src/test/resources/RestScheduleFinanse.csv")
     void testCalculateRestSchedule(int income, int expenses, int threshold) {
         RestSchedule service = new RestSchedule();
         int monthsOfRest = service.CalculateRestSchedule(income, expenses, threshold);
@@ -18,15 +22,17 @@ public class RestScheduleTest {
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "100000,60000,150000"
-    })
+/*      @CsvSource({
+             "100000,60000,150000"
+     })
+
+ */
+    @CsvFileSource(files = "src/test/resources/RestScheduleFinanseRich.csv")
     void testCalculateRestScheduleRich(int income, int expenses, int threshold) {
         RestSchedule service = new RestSchedule();
         int monthsOfRest = service.CalculateRestSchedule(income, expenses, threshold);
 
         Assertions.assertEquals(2, monthsOfRest);
     }
-
 
 }
